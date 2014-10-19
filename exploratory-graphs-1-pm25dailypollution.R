4c0a46505cb43a49d1194641a878f559a003b55c
Stanislav Bashkyrtsev <ctapobep@javatalks.ru>
2014-10-19 22:54:27 +0400

    initial

---
+++ exploratory-graphs-1-pm25dailypollution.R
@@ -0,0 +1,33 @@
+box = function(pollution) {
+  #boxplot(pollution$pm25, col = 'blue')
+  boxplot(pm25 ~ region, data = pollution, col = 'red')
+  abline(h = 12)
+}
+
+bar = function(pollution) {
+  barplot(table(pollution$region), col = 'wheat', 
+          main = "Number of counties in Each Region")
+}
+
+histo = function(pollution) {
+  par(mfrow = c(2,1), mar = c(4, 4, 2,1))
+  hist(subset(pollution, region == 'east')$pm25, col = 'green')
+  hist(subset(pollution, region == 'west')$pm25, col = 'green')
+  #hist(pollution$pm25, col = 'green', breaks = 50)
+  #rug(pollution$pm25)  
+}
+
+scatter = function(pollution) {
+  par(mfrow = c(1,2), mar = c(5,4,2,1))
+  with(subset(pollution, region == 'west'), plot(latitude, pm25, main = 'West'))
+  with(subset(pollution, region == 'east'), plot(latitude, pm25, main = 'East'))
+  abline(h = 12, lwd = 2, lty = 2)
+}
+
+pollution = read.csv('data/avgpm25.csv', colClasses = 
+                       c('numeric', 'character', 'factor', 'numeric', 'numeric'))
+summary(pollution$pm25)
+#box(pollution)
+#histo(pollution)
+#bar(pollution)
+scatter(pollution)
---
+++ latticeplotting.R
@@ -0,0 +1,8 @@
+library(lattice)
+library(datasets)
+# plots: xyplot (scatter), bwplot (box-n-whiskers), histogram, stripplot(boxplot with points)
+# plots: dotplot (dots on 'violin strings'), splom (scatterplot matrix like 'pairs' in basic plotting)
+# plots: levelplot, contourplot (plotting image data)
+airquality = transform(airquality, Month = factor(Month))
+plot = xyplot(Ozone ~ Wind | Month, data = airquality, layout = c(5, 1))
+print(plot)
